6c3a566934ab36542b64fdbf19e0985da00da361
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-03-25 00:35:37 +0100

    done exercise 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,31 @@
-alert("hello!");
+const button = document.querySelector("#btn");
+button.style.backgroundColor = 'green'
+const inputSelect = document.querySelector("#search-text");
+inputSelect.addEventListener("input", (e) => {
+  e.preventDefault();
+  inputSelect.innerText = "Hello World";
+ 
+});
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Someone clicked me");
+});
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Someone clicked me twice");
+});
+/*
+const headerTitle = document.querySelector(".header__title");
+console.log("title", headerTitle);
+const headerSubtitle = document.querySelector(".header__subtitle");
+console.log("subtitle", headerSubtitle);
+const showUnfinished = document.querySelector("#show-unfinished");
+console.log("label unfinished", showUnfinished);
+button.style.backgroundColor = "green";
+*/
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+
+
+
+
+
