9f897f51212ddff137738f7916a9ec3b734a25c8
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-03-25 12:35:37 +0100

    add alert

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,17 @@
 const button = document.querySelector("#btn");
 button.style.backgroundColor = 'green'
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert('thanku')
+});
 const inputSelect = document.querySelector("#search-text");
 inputSelect.addEventListener("input", (e) => {
   e.preventDefault();
   inputSelect.innerText = "Hello World";
  
 });
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("Someone clicked me");
-});
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("Someone clicked me twice");
-});
+
+
 /*
 const headerTitle = document.querySelector(".header__title");
 console.log("title", headerTitle);
